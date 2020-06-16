4ea8b6852a44b10d03ab80c993551cc5d96ca9ba
macjabeth <macbethjonathan@gmail.com>
2020-06-16 09:53:43 -0400

    complete search algorithms

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,22 @@
+# O(n) runtime | O(1) space
 def linear_search(arr, target):
-    # Your code here
-
-
+    for index, value in enumerate(arr):
+        if value == target:
+            return index
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        value = arr[middle]
+        if value == target:
+            return middle
+        elif target < value:
+            right = middle - 1
+        else:
+            left = middle + 1
     return -1  # not found
